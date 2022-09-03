9a0fef9c535f83b9053242f9e1443ab50e2ae5ac
weapecet <weapecet@gmail.com>
2022-09-03 14:22:55 +0200

    Pierwsze PageFactory

--- src/test/java/pl/coderslab/model/HotelMainPage.java
+++ src/test/java/pl/coderslab/model/HotelMainPage.java
@@ -3,6 +3,8 @@ package pl.coderslab.model;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
@@ -10,23 +12,39 @@ public class HotelMainPage {
 
     private WebDriver driver;
 
+    @FindBy(className = "user_login")
+    private WebElement signInButton;
+
+    @FindBy(xpath = "/html//form[@id='search_hotel_block_form']//button[@type='button']")
+    private WebElement selectHotelDropdown;
+
+    @FindBy(xpath = "//form[@id='search_hotel_block_form']//ul[@class='dropdown-menu hotel_dropdown_ul']")
+    private List<WebElement> hotels;
+
+    @FindBy(id = "check_in_time")
+    private WebElement checkInDateInput;
+
+    @FindBy(id = "check_out_time")
+    private WebElement checkOutDateInput;
+
+    @FindBy(id = "search_room_submit")
+    private WebElement searchNowButton;
+
     public HotelMainPage(WebDriver driver){
+        PageFactory.initElements(driver, this);
         this.driver = driver;
     }
 
     public void clickSignIn(){
-        WebElement signInButton = this.driver.findElement(By.className("user_login"));
         signInButton.click();
     }
 
     public void searchForHotelRoomsBetweenDates(String hotelName, String fromDate, String toDate) {
-        WebElement selectHotelDropdown = this.driver.findElement(By.xpath("/html//form[@id='search_hotel_block_form']//button[@type='button']"));
         selectHotelDropdown.click();
 
 //        WebElement hotel = this.driver.findElement(By.xpath("//form[@id='search_hotel_block_form']//ul[@class='dropdown-menu hotel_dropdown_ul']/li"));
 //        hotel.click();
 
-        List<WebElement> hotels = this.driver.findElements(By.xpath("//form[@id='search_hotel_block_form']//ul[@class='dropdown-menu hotel_dropdown_ul']"));
         for(WebElement hotel: hotels){
             if (hotel.getText().equals(hotelName)){
                 hotel.click();
@@ -34,14 +52,11 @@ public class HotelMainPage {
             }
         }
 
-        WebElement checkInDateInput = this.driver.findElement(By.id("check_in_time"));
-        WebElement checkOutDateInput = this.driver.findElement(By.id("check_out_time"));
         checkInDateInput.clear();
         checkInDateInput.sendKeys(fromDate);
         checkOutDateInput.clear();
         checkOutDateInput.sendKeys(toDate);
 
-        WebElement searchNowButton = this.driver.findElement(By.id("search_room_submit"));
         searchNowButton.click();
     }
 }
